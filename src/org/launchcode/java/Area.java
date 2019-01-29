379e3e74acffbb7f6bf1ef1af766f793e804fd6e
robE357 <rob.ellis357@gmail.com>
2019-01-28 20:30:54 -0500

    fixed stuff

--- src/org/launchcode/java/Area.java
+++ src/org/launchcode/java/Area.java
@@ -5,18 +5,25 @@ import java.util.Scanner;
 public class Area {
 
     public static void main(String[] args) {
-        double Area;
+        double area = 0;
         double pi = 3.14;
         double radius;
 
-        System.out.println("Enter the radius of the circle:");
-        Scanner scan = new Scanner(System.in);
-        radius = scan.nextDouble();
-        Area = pi * radius * radius;
-
-        System.out.print("The area of the circle is: " + Area);
 
 
+        while (area == 0) {
 
+            System.out.println("Enter the radius of the circle:");
+            Scanner scan = new Scanner(System.in);
+
+            radius = scan.nextDouble();
+
+            if (radius <= 0) {
+                System.out.println("Only Positive Numbers & no Letters Please!");
+            } else {
+                area = pi * radius * radius;
+                System.out.print("The area of the circle is: " + area);
+            }
+        }
     }
 }
